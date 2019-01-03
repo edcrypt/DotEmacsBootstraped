d07bb3b13e576814c328a46594c4d38750e72b20
Eduardo de Oliveira Padoan <eduardo.padoan@lendico.com.br>
2019-01-03 16:10:17 -0200

    Initial commit -- created from emacs-bootstrap.com

---
+++ .custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-strict-inconsistent-return-warning nil)
+ '(js2-strict-missing-semi-warning nil)
+ '(package-selected-packages
+   (quote
+    (js2-refactor js-comint company-tern tern js2-mode pyenv-mode py-autopep8 pip-requirements elpy yasnippet wgrep which-key undo-tree smex smartparens org-bullets org-projectile neotree multiple-cursors magit hlinum helm-swoop helm-projectile helm-git-grep helm-ag helm flycheck expand-region exec-path-from-shell dashboard company ace-jump-mode spacemacs-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
